c5b84bbd32b79a913a60e7656f8bb205c1630966
Alan <alansal_11@hotmail.com>
2020-09-26 21:53:53 -0500

    Added the TodoProvider, TodoInput and TodoLists components

--- pages/todo.js
+++ pages/todo.js
@@ -1,10 +1,13 @@
-
+import {TodoProvider} from '../Context/Todo';
+import TodoList from '../Components/Todo/TodoList';
+import TodoInput from '../Components/Todo/TodoInput';
 
 const Todo = () => {
-    return (
-        <>
-            hola
-        </>
+    return(
+        <TodoProvider>
+            <TodoInput />
+            <TodoList />
+        </TodoProvider>
     );
 };
 
