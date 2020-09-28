dcee80e5422b2346727d43e2ec07adb4f38dd358
Alan <alansal_11@hotmail.com>
2020-09-28 17:59:34 -0500

    Added COLORS and QUERIES

---
+++ styles/style.js
@@ -0,0 +1,12 @@
+export const COLORS = {
+    primary: '#010A20',
+    secondary: '#262E41',
+    tertiary: '#3B4253',
+    textColor: '#FFF7F5',
+    buttonColor: '#FE8368'
+}
+
+export const QUERIES = {
+    small: `max-width: 767px`,
+    medium: `max-width: 1023px`,
+}
