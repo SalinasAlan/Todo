a76023eda7b68342dc8302b979bbeb088e9ed40d
Alan <alansal_11@hotmail.com>
2020-09-28 11:25:44 -0500

    The styled of the input and button was added

---
+++ Components/Todo/InputStyle.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Input = styled.input`
+    width: 70%;
+    height: 70px;
+    margin-bottom: 30px;
+    color: #FFF7F5;
+    padding: 10px;
+    font-size: 2em;
+    border-radius: 20px;
+    background: #3B4253;
+    border: none;
+    outline: none;
+`;
+
+export const ButtonAdd = styled.button`
+    width: 50px;
+    height: 50px;
+    margin-left: 20px;
+    font-size: 2em;
+    font-weight: bold;
+    background: #FE8368;
+    border-radius: 50%;
+    color: #FFF7F5;
+    cursor: pointer;
+    border: none;
+    outline: none;
+`;
