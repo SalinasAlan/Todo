c6358dedf364476dc57c2af30d813eee2e71d875
Alan <alansal_11@hotmail.com>
2020-09-28 10:50:27 -0500

    The style of containers was created

---
+++ styles/Containers/Container.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    width: 50%;
+    height: 70%;
+    margin: auto;
+    padding: 30px;
+    border-radius: 30px;
+    color: #FFF7F5;
+    background: #262E41;
+`; 
+
+export const ListContainer = styled.div`
+    width: 100%;
+`;
