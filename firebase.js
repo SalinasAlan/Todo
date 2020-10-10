1d56efdb007e22ed69121fb35c3c6eac39af8580
Alan <alansal_11@hotmail.com>
2020-10-10 09:23:02 -0500

    environment variables were added

--- firebase.js
+++ firebase.js
@@ -2,16 +2,15 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore'
 
-var config = {
-    apiKey: "AIzaSyANRfpJiHOYZ-Nz4NojDerM7odRyn_1l9Y",
-    authDomain: "todo-53a87.firebaseapp.com",
-    databaseURL: "https://todo-53a87.firebaseio.com",
-    projectId: "todo-53a87",
-    storageBucket: "todo-53a87.appspot.com",
-    messagingSenderId: "396023480811",
-    appId: "1:396023480811:web:4b43affd113f2acfbe19ae",
-    measurementId: "G-19GZDXEV3X"
-};
-
+const config = {
+    apiKey: process.env.FIREBASE_KEY,
+    authDomain: process.env.FIREBASE_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_SENDER_ID,
+    appId: process.env.FIREBASE_APPID,
+    measurementId: process.env.FIREBASE_MEASUREMENTID
+}
 
 export default !firebase.apps.length ? firebase.initializeApp(config) : firebase.app();
