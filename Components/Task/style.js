8d35e10cbc3d47cc972b6dc9d48f12f9a89f54cc
Alan <alansal_11@hotmail.com>
2020-10-11 09:04:19 -0500

    Added style to task components

--- Components/Task/style.js
+++ Components/Task/style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { QUERIES } from '../../styles/style';
+import { QUERIES, COLORS } from '../../styles/style';
 
 export const Checbox = styled.input`
     background-color: transparent;
@@ -7,9 +7,12 @@ export const Checbox = styled.input`
     background: #9CE3B4;
 `;
 
-export const TaskText = styled.span`
-    margin-left:10px;
+export const TaskText = styled.p`
+    margin:0 0 0 10px;
     font-size: 1.5em;
+    display: inline-block;
+    grid-area: text;
+    text-decoration: ${props => props.complete ? "line-through" : null};
 
     @media only screen and (${QUERIES.medium}) {
         font-size: 2em;
@@ -19,4 +22,32 @@ export const TaskText = styled.span`
         font-size: 1.5em;
     }
 
+`;
+
+export const TaskContainer = styled.div`
+    width: 100%;
+    display: grid;
+    grid-template-columns: 90% 10%;
+    grid-template-areas: 
+    "task button";
+`;
+
+export const TaskWrapper = styled.div`
+    display: grid;
+    grid-template-columns: 5% 95%;
+    grid-template-areas: 
+    "checkbox text";
+    align-items: center;
+    grid-area: task;
+`;
+
+export const Button = styled.button`
+    margin: 0;
+    border: none;
+    grid-area: button;
+    background-color: transparent;
+    /* float: right; */
+    color: ${COLORS.buttonColor};
+    cursor: pointer;
+    outline: none;
 `;
