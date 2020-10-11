a4794308c886e44b850f3443376dc434d76a068a
Alan <alansal_11@hotmail.com>
2020-10-11 00:22:09 -0500

    The layout display direction was modified

--- Components/Layout.js
+++ Components/Layout.js
@@ -5,6 +5,7 @@ export const Layout = styled.div`
     width: 100vw;
     height: 100vh; 
     display: flex;
+    flex-direction: column;
     align-items: center;
     background: ${COLORS.primary};
 `;
