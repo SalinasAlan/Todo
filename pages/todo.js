78aa52e0afbf597bc15e2d56e1c176c82edf33af
Alan <alansal_11@hotmail.com>
2020-10-11 09:06:19 -0500

    Added the header component

--- pages/todo.js
+++ pages/todo.js
@@ -3,11 +3,13 @@ import TodoList from '../Components/Todo/TodoList';
 import TodoInput from '../Components/Todo/TodoInput';
 import { Layout } from '../Components/Layout';
 import { Container } from '../styles/Containers/Container'
+import Header from '../Components/Header/Header';
 
 const Todo = () => {
     return (
         <Layout>
             <TodoProvider>
+                <Header />
                 <Container>
                     <TodoInput />
                     <TodoList />
