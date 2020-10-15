fca66186eff74d8daa76ea45e5da0bf0ae597c8d
Alan <alansal_11@hotmail.com>
2020-10-14 21:33:18 -0500

    Updated the todo provider

--- Context/Todo.js
+++ Context/Todo.js
@@ -1,35 +1,72 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import { v4 } from 'uuid';
+import app from '../firebase';
 
 const TodoContext = React.createContext();
 
 export const useTasks = () => useContext(TodoContext);
 
-export const TodoProvider = ({ children }) => {
+export const TodoProvider = ({ children, id }) => {
 
     const [tasks, setTasks] = useState([]);
 
+    useEffect(() => {
+        app
+            .firestore()
+            .collection('list')
+            .doc(`${sessionStorage.getItem('id')}`)
+            .collection('tasks')
+            .orderBy('createdAt')
+            .onSnapshot(data => {
+                const tasksData = data.docs.map(doc => ({ ...doc.data() }))
+                setTasks(tasksData);
+            })
+    }, [])
+
     const addTask = task => {
-        setTasks([
-            ...tasks,
-            {
-                id: v4(),
-                task,
-                complete: false
-            }
-        ])
+
+        const newTask = {
+            id: v4(),
+            task,
+            complete: false,
+            createdAt: new Date()
+        }
+
+        app
+            .firestore()
+            .collection('list')
+            .doc(`${sessionStorage.getItem('id')}`)
+            .collection('tasks')
+            .doc(newTask.id)
+            .set(newTask)
     }
 
     const setStatusTask = (id, status) => {
-        setTasks(tasks.map(t => t.id === id ? { ...t, complete: status } : t))
+        app
+            .firestore()
+            .collection('list')
+            .doc(`${sessionStorage.getItem('id')}`)
+            .collection('tasks')
+            .doc(id)
+            .update({
+                complete: status
+            });
     }
 
     const handleDeteleTask = (idDeleted) => {
-        setTasks(
-            tasks.filter(task => {
-                return task.id != idDeleted;
-            })
-        )
+        // setTasks(
+        //     tasks.filter(task => {
+        //         return task.id != idDeleted;
+        //     })
+        // )
+
+        app
+            .firestore()
+            .collection('list')
+            .doc(`${sessionStorage.getItem('id')}`)
+            .collection('tasks')
+            .doc(idDeleted)
+            .delete()
     }
 
     return (
