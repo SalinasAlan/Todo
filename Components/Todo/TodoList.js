e9da45fa1405f22b55ca73a8e94d29d753bd1a9b
Alan <alansal_11@hotmail.com>
2020-10-14 21:31:09 -0500

    Updated the TodoList component

--- Components/Todo/TodoList.js
+++ Components/Todo/TodoList.js
@@ -1,19 +1,37 @@
+import { useState } from 'react';
 import { useTasks } from '../../Context/Todo';
 import Task from '../Task/Task';
 import { ListContainer } from '../../styles/Containers/Container';
+import { ShowTask } from './TodoListStyle';
+import { HiddenTasksContainer } from '../../styles/Containers/Container';
 
 const TodoList = () => {
 
     const { tasks } = useTasks();
+    const [show, setShow] = useState(false);
 
     return (
         <>
             <ListContainer>
                 {
-                    tasks.map((task, id) => (
-                        <Task key={id} {...task} />
-                    ))
+                    tasks.map((task, id) => {
+                        if (!task.complete) {
+                            return <Task key={id} {...task} />
+                        }
+                    })
                 }
+                <ShowTask onClick={_ => setShow(!show)}>
+                    show completed task
+                </ShowTask>
+                <HiddenTasksContainer show={show}>
+                    {
+                        tasks.map((task, id) => {
+                            if (task.complete) {
+                                return <Task key={id} {...task} completed checked />
+                            }
+                        })
+                    }
+                </HiddenTasksContainer>
             </ListContainer>
         </>
     )
